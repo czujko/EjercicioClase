12558280e58924c38018456b47e7903ea7c059e1
czujko <55107723+czujko@users.noreply.github.com>
2022-01-13 11:16:01 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+nombre = input("Introduce un nombre: ")
+if nombre == "0":
+    print("No has introducido ningún nombre")
+else:
+    lista = []
+    lista.append(nombre)
+    nombre = input("Introduce un nombre: ")
+    while nombre != "":
+        lista.append(nombre)
+        nombre = input("Introduce un nombre: ")
+    print("Lista de nombres:", lista)
+
+nombre = input("Introduce un nombre para eliminar: ")
+if nombre == 0:
+    print("No has introducido ningún nombre")
+else:
+    lista.remove(nombre)
+    print("Lista de nombres:", lista)
