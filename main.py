2db585bc15a3c7c2f9998ff0cf7b3e25171af2e6
czujko <55107723+czujko@users.noreply.github.com>
2022-01-13 11:33:10 +0100

    Optimizado + capitalize()

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 lista = []
-nombre = input("Introduce un nombre: ").lower()
+nombre = input("Introduce un nombre: ").capitalize()
 while nombre != "0":
     lista.append(nombre)
-    nombre = input("Introduce un nombre: ").lower()
+    nombre = input("Introduce un nombre: ").capitalize()
 print("Lista de nombres:", lista)
 
-nombre = input("Introduce un nombre para eliminar: ").lower()
+nombre = input("Introduce un nombre para eliminar: ").capitalize()
 while nombre != '0':
     lista.remove(nombre)
-    nombre = input("Introduce un nombre para eliminar: ").lower()
+    nombre = input("Introduce un nombre para eliminar: ").capitalize()
 
 print("Lista de nombres:", lista)
